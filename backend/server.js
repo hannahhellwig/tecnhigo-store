a9a028e2659d98371be70bd88f0448741b718f7e
hannahhellwig <39677202+hannahhellwig@users.noreply.github.com>
2018-10-31 13:30:36 +0100

    created api

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,9 @@ app.use(bodyParser.json())
 
 app.use(cors())
 
-const mongoServer = "mongo://localhost/myNewDB"
+const mongoServer = "mongodb://localhost/myFyndiqDb"
 
-mongoose.connect(mongoServer, { useNewUrlParser: true })
+mongoose.connect(mongoServer, { useMongoClient: true })
 mongoose.Promise = Promise
 
 mongoose.connection.on("error", err => console.error("Connection error:", err))
@@ -19,9 +19,7 @@ mongoose.connection.once("open", () => console.log("Connected to mongodb"))
 
 const Product = mongoose.model("Product", {
   itemID: Number,
-  sellerID: String,
   title: String,
-  description: String,
   price: Number,
   image: String,
   category: String,
@@ -29,7 +27,24 @@ const Product = mongoose.model("Product", {
 })
 
 app.get("/", (req, res) => {
-  res.send("Team 1 API")
+  res.send("Team 4 API")
 })
 
-app.listen(8080, () => console.log("Team 1 API listening on port 8080!"))
+app.get("/products", (req, res) => {
+  Product.find().then(products => {
+    console.log("products: ", products)
+    res.json(products)
+  })
+})
+
+// Endpoint to create a product. Send a POST to /products with a JSON body
+// with the keys and values you want to persist in the database.
+app.post("/products", (req, res) => {
+  const product = new Product(req.body)
+
+  product.save()
+    .then(() => { res.status(201).send("Product created") })
+    .catch(err => { res.status(400).send(err) })
+})
+
+app.listen(8080, () => console.log("Team 4 API listening on port 8080!"))
