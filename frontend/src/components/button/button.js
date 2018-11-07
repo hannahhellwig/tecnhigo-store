b54efeb2ac78836c135f31d56bab9630c9121e28
louisebohlin <37780677+louisebohlin@users.noreply.github.com>
2018-11-07 15:31:19 +0100

    Styling on button component

--- frontend/src/components/button/button.js
+++ frontend/src/components/button/button.js
@@ -1,15 +1,13 @@
-import React, { Component } from "react";
-import "./button.scss";
+import React, { Component } from "react"
+import "./button.scss"
 
-
-class Button extends Component{
-  render(){
-
-    return(
+class Button extends Component {
+  render() {
+    return (
 
       <button onClick={this.props.onClick} className="button">{this.props.children}</button>
-    );
+    )
   }
 }
 
-export default Button;
+export default Button
